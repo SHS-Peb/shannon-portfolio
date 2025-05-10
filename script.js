02349fac17ab3119af17eb60e47f62e3274a6000
unknown <blackskiesdancer@gmail.com>
2025-05-10 19:57:08 +1000

    theme switch javascript code initialised

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById("theme-switch")
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', "active")
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode")
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+if(darkmode === "active") enableDarkmode()
+
 document.addEventListener("DOMContentLoaded", () => {
     // Smooth scrolling for internal links
     document.querySelectorAll("nav a").forEach(anchor => {
@@ -24,4 +44,5 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
 
